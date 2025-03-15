85639329d5ba651a93e5eb48ac84380537c8475a
Victor Farias <gfvictor@proton.me>
2025-03-16 06:48:45 +0900

    feat: created services api

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { environment } from "../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getData(endpoint: string): Observable<any> {
+    return this.http.get<string>(`${this.apiUrl}/${endpoint}`);
+  }
+
+  postData(endpoint: string, data: any): Observable<any> {
+    return this.http.post<string>(`${this.apiUrl}/${endpoint}`, data);
+  }
+}
