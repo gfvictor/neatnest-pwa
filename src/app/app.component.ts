7b0f1c6153812a0a5569a051493a696d05b28ff4
Victor Farias <gfvictor@proton.me>
2025-03-16 21:45:58 +0900

    chore: defined as standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  standalone: true,
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'neatnest-pwa';
+    title = 'neatnest-pwa';
 }
