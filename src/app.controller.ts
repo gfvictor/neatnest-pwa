c114b209cf1a0b2160aeea6ea3b7e71380b83052
Victor Farias <gfvictor@proton.me>
2025-08-06 18:54:16 +0900

    feat: defined app module e controller

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get()
+  getRoot() {
+    return {
+      message: 'Neatnest API is working!',
+      version: '1.0.0'
+    };
+  }
+}
