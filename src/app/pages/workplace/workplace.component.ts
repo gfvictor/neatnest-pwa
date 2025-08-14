606dcda886949ec38ad139d46d5269d917b03b6d
Victor Farias <gfvictor@proton.me>
2025-08-14 16:26:20 +0900

    feat: created workplace components

---
+++ src/app/pages/workplace/workplace.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: 'app-workplace',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './workplace.component.html',
+  styleUrl: './workplace.component.css'
+})
+export class WorkplaceComponent {
+
+}
