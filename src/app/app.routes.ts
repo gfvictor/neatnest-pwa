ed1fa838eed384dc019c35b9b35fc815b375585f
Victor Farias <gfvictor@proton.me>
2025-10-24 08:42:46 +0900

    chore: reflected new "components" directory to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,36 +3,37 @@ import { Routes } from "@angular/router";
 export const routes: Routes = [
   {
     path: "",
-    loadComponent: () => import("./pages/home/home.component").then((m) => m.HomeComponent),
+    loadComponent: () => import("./components/home/home.component").then((m) => m.HomeComponent),
   },
 
   {
     path: "login",
-    loadComponent: () => import("./pages/login/login.component").then((m) => m.LoginComponent),
+    loadComponent: () => import("./components/login/login.component").then((m) => m.LoginComponent),
   },
   {
     path: "dashboard",
     loadComponent: () =>
-      import("./pages/dashboard/dashboard.component").then((m) => m.DashboardComponent),
+      import("./components/dashboard/dashboard.component").then((m) => m.DashboardComponent),
   },
 
   {
     path: "household",
     loadComponent: () =>
-      import("./pages/household/household.component").then((m) => m.HouseholdComponent),
+      import("./components/household/household.component").then((m) => m.HouseholdComponent),
   },
   {
     path: "room/:id",
-    loadComponent: () => import("./pages/room/room.component").then((m) => m.RoomComponent),
+    loadComponent: () => import("./components/room/room.component").then((m) => m.RoomComponent),
   },
   {
     path: "container/:containerId",
     loadComponent: () =>
-      import("./pages/container/container.component").then((m) => m.ContainerComponent),
+      import("./components/container/container.component").then((m) => m.ContainerComponent),
   },
 
   {
     path: "object/:id",
-    loadComponent: () => import("./pages/object/object.component").then((m) => m.ObjectComponent),
+    loadComponent: () =>
+      import("./components/object/object.component").then((m) => m.ObjectComponent),
   },
 ];
