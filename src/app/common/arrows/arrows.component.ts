5e408925e04caaf47c978c59941145a5e7606a5e
Victor Farias <gfvictor@proton.me>
2025-10-30 11:34:28 +0900

    feat: created arrows watermark components

---
+++ src/app/common/arrows/arrows.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { CommonModule, NgOptimizedImage } from "@angular/common";
+
+@Component({
+  selector: "app-arrows",
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage],
+  templateUrl: "./arrows.component.html",
+})
+export class ArrowsComponent {}
