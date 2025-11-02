d669f970531417e7aecd1eb261281ffd31c59205
Victor Farias <gfvictor@proton.me>
2025-11-02 11:52:04 +0900

    fix: corrected api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // apiUrl: "https://neatnest.vercel.app",
-  // apiUrl: "https://neatnest-api.onrender.com",
-  apiUrl: "/api",
+  apiUrl: "https://neatnest-api.onrender.com",
+  // apiUrl: "/api",
 };
