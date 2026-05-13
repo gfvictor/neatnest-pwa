8ff983b28e17dad2bf1f3957becc5ecb4e78e138
Victor Farias <gfvictor@proton.me>
2026-05-13 18:42:56 +0900

    chore: add new register route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,10 @@ export const routes: Routes = [
     loadComponent: () => import("./components/login/login.component").then((m) => m.LoginComponent),
   },
   {
+    path: "register",
+    loadComponent: () => import("./components/register/register.component").then((m) => m.RegisterComponent),
+  },
+  {
     path: "dashboard",
     loadComponent: () =>
       import("./components/dashboard/dashboard.component").then((m) => m.DashboardComponent),
