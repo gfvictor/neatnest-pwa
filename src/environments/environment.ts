5116022eb40ada33e8368059fc8212cafea6fbc7
Victor Farias <gfvictor@proton.me>
2026-05-16 20:17:07 +0900

    hotfix: resolve crashing caused by wrong apiUrl variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: "https://https://neatnest-api-twqwo.ondigitalocean.app/",
+  apiUrl: "https://neatnest-api-twqwo.ondigitalocean.app",
   supabaseUrl: "https://iniucwughxqcnkncusfw.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluaXVjd3VnaHhxY25rbmN1c2Z3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4MTA3MDUsImV4cCI6MjA1NDM4NjcwNX0.y-_RoyEO_izDiclMGCQtM74Gb7U8u0ZsgI3jsGqSNv8",
 };
